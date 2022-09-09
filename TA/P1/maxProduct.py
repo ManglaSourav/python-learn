b47a17da6a278a23061030339ea7dfcb92f60868
Sourav Mangla <mangla.sourav96@gmail.com>
2022-09-08 19:56:35 -0700

    initial

---
+++ TA/P1/maxProduct.py
@@ -0,0 +1,9 @@
+'''
+Problem Statement: Given an array of integers A and an integer m, 
+determine the maximum product of m consecutive integers in the array.(In O(N+M))
+Example:
+A = [3, 1, 6, 2, 8, 9, 3, 4], m = 3
+Answer: 216 (which is the product of 8, 9, and 3)
+'''
+
+print("hello")
---
+++ TA/P1/rearrange.py
@@ -0,0 +1,12 @@
+'''
+Problem Statement: Given an array of integers A, rearrange A so that all the 0’s are
+pushed to the end of the array. All other values should stay in the same relative
+order. (In O(N))
+Examples:
+A = [2, 6, 1, 9, 0, 2]
+Result: [2, 6, 1, 9, 2, 0]
+A = [0, 0, 6, 1, 0, 9, 8]
+Result: [6, 1, 9, 8, 0, 0, 0]
+A = [3, 1, 4, 1, 5, 9, 2]
+Result: [3, 1, 4, 1, 5, 9, 2]
+'''
